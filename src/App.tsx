9b755b1ea8c35b0d8b62e4a84de288a086aaa58c
jagadeesh-652 <jagadeesh@schemaxtech.com>
2022-01-31 12:45:15 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+//import { DatePicker } from 'antd';
+import React from 'react';
+//import BigForm from './antdcomponents/bigform';
+//import WrappedTimeRelatedForm from './antdcomponents/gun';
+import Inlinelogin from './antdcomponents/inlinelogin';
+
+//import Bigform from './antdcomponents/bigform';
+//import LoginForm from './antdcomponents/loginform';
+
+  
+
+
+
+function App() {
+  return (
+   //<WrappedTimeRelatedForm/>
+  // <BigForm/>
+  // <LoginForm/>
+  <Inlinelogin/>
+  )
+}
+
+export default App;
+
+
